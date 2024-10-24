78df66b143a7b52cefc52b3b379d367ba87d6016
JorgeE93 <105331520+JorgeE93@users.noreply.github.com>
2024-10-24 13:48:16 -0700

    added state and compositestate.cpp

---
+++ src/CompositeState.cpp
@@ -0,0 +1,10 @@
+// CompositeState.cpp
+#include "pyflow/CompositeState.h"
+#include "pyflow/StateMachine.h"
+
+namespace pyflow {
+
+// Implementation of CompositeState methods, if any
+
+} // namespace pyflow
+
---
+++ src/State.cpp
@@ -0,0 +1,10 @@
+// State.cpp
+#include "pyflow/State.h"
+#include "pyflow/StateMachine.h"
+
+namespace pyflow {
+
+// Implementation of State methods, if any
+
+} // namespace pyflow
+
